d34e996a4ad1071fac55cf44d48d476f9ff5ed78
Herbert Caffarel <herbert.caffarel@cicef.pro>
2024-06-04 16:58:15 +0000

    Fichier module CSS

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css'
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className={styles.App}>
+      <header className={styles['App-header']}>
         <p>Éditez <code>src/App.js</code> et sauvegardez pour recharger</p>
       </header>
     </div>
