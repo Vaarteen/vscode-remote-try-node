1896bbc03c247f4b433ff237ece92feb72a0d732
Herbert Caffarel <herbert.caffarel@cicef.pro>
2024-06-04 16:37:02 +0000

    Exemple d'application de base avec état

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,36 +1,19 @@
-import React, { useState, useEffect } from 'react';
-
-function ExampleWithUseEffect() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    /* modification du titre de l'onglet, donc interaction avec
-    l'environnement : la fonction n'est pas pure */
-    document.title = `Vous avez cliqué ${count} fois`;
-  });
-  return (
-    <div>
-      <p>Vous avez cliqué {count} fois</p>
-      <button onClick={() => setCount(count + 1)}> Cliquez ici </button>
-    </div>
-  );
-}
-
-
-function Welcome(props) {
-  return <h1>Bonjour, {props.nom}</h1>;
-}
-
-function Main() {
-  return (
-    <main>
-      <Welcome nom="Bob" />
-      <ExampleWithUseEffect />
-    </main>
-  )
-}
+import React, { useState } from 'react';
+import Welcome from './Welcome';
 
 function App() {
-  return <Main />;
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <header>
+        <Welcome name="Bob" />
+      </header>
+      <main>
+        <p>Vous avez cliqué {count} fois</p>
+        <button onClick={() => setCount(count + 1)}> Cliquez ici </button>
+      </main>
+    </div>
+  )
 }
 
 export default App;
