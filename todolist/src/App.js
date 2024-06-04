65ac8e09b1c3f6ca00d97ce581804edf4bfdf063
Herbert Caffarel <herbert.caffarel@cicef.pro>
2024-06-04 17:00:48 +0000

    CSS avec bibliothèque styles-components

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import styles from './App.module.css'
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 function App() {
   return (
-    <div className={styles.App}>
-      <header className={styles['App-header']}>
+    <AppWrapper>
+      <Header>
         <p>Éditez <code>src/App.js</code> et sauvegardez pour recharger</p>
-      </header>
-    </div>
+      </Header>
+    </AppWrapper>
   );
 }
 export default App;
