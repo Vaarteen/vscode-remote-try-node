3c76f829af6aa14fde50d859209383f06a8b62e8
Herbert Caffarel <herbert.caffarel@cicef.pro>
2024-06-04 18:25:21 +0000

    POST

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from 'react';
+
 function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [post, setPost] = useState('');
+  const createPost = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+        title: 'My title',
+        body: post,
+        userId: 1
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => {
@@ -29,6 +51,10 @@ function App() {
   return (
     <div>
       <h1>Posts</h1>
+      <div>
+        <input type='text' name='post' value={post} onChange={(e) => setPost(e.target.value)} />
+        <button onClick={createPost}>Envoyer</button>
+      </div>
       <ul>
         {data.map((post) => (
           <li key={post.id}>{post.id}. {post.title}</li>
