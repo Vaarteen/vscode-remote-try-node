e37a92ab6c2296268614f5c7dbecd837e147317e
Herbert Caffarel <herbert.caffarel@cicef.pro>
2024-06-04 15:42:41 +0000

    Utilisation d'état avec cumulk des effets

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,30 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  // déclaration d’un état avec variable count = 0 et setter setCount
+  const [count, setCount] = useState(0);
+  function onClickAction() {
+    setCount((c) => c + 1);
+    setCount((c) => c + 1);
+    setCount((c) => c + 1);
+  }
+  return (
+    <div>
+      <p>Vous avez cliqué {count} fois</p>
+      <button onClick={onClickAction}> +3 </button>
+    </div>
+  );
+}
+
 function Welcome(props) {
   return <h1>Bonjour, {props.nom}</h1>;
 }
+
 function Main() {
   return (
     <main>
       <Welcome nom="Bob" />
+      <Counter />
     </main>
   )
 }
